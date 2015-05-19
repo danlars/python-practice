4498f57a223c6da2fb46af1277a63f917281dd6f
Notreu <mbn@notreu>
2015-05-19 22:24:36 +0200

    git test

--- hangman/main.py
+++ hangman/main.py
@@ -1,4 +1,5 @@
 
 # Install PyMySQL --------- pip3 install PyMySQL
 import pymysql
-conn = pymysql.connect(db='portfolio', user='python', passwd='python', host='localhost')
+conn = pymysql.connect(db='python-hangman', user='python', passwd='python', host='localhost')
+#mbn git test
