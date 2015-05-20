7cd653568413cce1190edf8b13f642e390838459
daniel <daniellarsen725@hotmail.com>
2015-05-20 08:45:13 +0200

    mysql fetch and edited database

--- hangman/main.py
+++ hangman/main.py
@@ -1,4 +1,16 @@
 
 # Install PyMySQL --------- pip3 install PyMySQL
 import pymysql
-conn = pymysql.connect(db='portfolio', user='python', passwd='python', host='localhost')
+conn = pymysql.connect(db='python-hangman', user='python', passwd='python', host='localhost')
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM dictionary")
+
+print(cur.description)
+
+for row in cur:
+   print(row)
+
+cur.close()
+conn.close()
--- schema/dictionary.sql
+++ schema/dictionary.sql
@@ -1,38 +1,54 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `python-hangman` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_danish_ci */;
+USE `python-hangman`;
+-- MySQL dump 10.13  Distrib 5.6.19, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: May 18, 2015 at 09:59 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: python-hangman
+-- ------------------------------------------------------
+-- Server version	5.6.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `python-hangman`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `dictionary`
 --
 
-CREATE TABLE IF NOT EXISTS `dictionary` (
+DROP TABLE IF EXISTS `dictionary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dictionary` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `word` varchar(20) COLLATE utf8_danish_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `word` (`word`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `dictionary`
+--
+
+LOCK TABLES `dictionary` WRITE;
+/*!40000 ALTER TABLE `dictionary` DISABLE KEYS */;
+INSERT INTO `dictionary` VALUES (4,'Maxima'),(2,'Saks'),(1,'this is word'),(3,'Viking');
+/*!40000 ALTER TABLE `dictionary` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-20  8:43:27
