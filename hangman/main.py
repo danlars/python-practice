ee91def0c427a3c784eceebdf2342fb8ddc8caf8
daniel <daniellarsen725@hotmail.com>
2015-05-26 14:42:01 +0200

    TODO

--- hangman/main.py
+++ hangman/main.py
@@ -8,10 +8,19 @@ cur = conn.cursor()
 cur.execute("SELECT word FROM dictionary ORDER BY RAND() LIMIT 1")
 
 # print(cur.description)
+random_string = cur.fetchone()[0]
 
-print(cur.fetchone()[0])
+
+#TODO: get number of characters from string and insert as _ in console
+#TODO: After each guess, if correct, remove _ and replace with the guessed character at each respective character placement
+#TODO: Maximum of 10 guesses (vi kan altid lave det om)
+#TODO: Funsies, draw a hangman in console
+
 # for row in cur:
 #    print(row)
+
+
+
 
 cur.close()
 conn.close()
