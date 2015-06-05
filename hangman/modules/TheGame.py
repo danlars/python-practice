146282dc1a08d198b7a4d492b6a81f6ad5c7d255
Notreu <mbn@notreu>
2015-06-05 15:23:27 +0200

    added tries + check if guess is in word

--- hangman/modules/TheGame.py
+++ hangman/modules/TheGame.py
@@ -4,8 +4,6 @@ import pymysql
 conn = pymysql.connect(db='python-hangman', user='python', passwd='python', host='localhost')
 cur = conn.cursor()
 
-
-
 def hangmanTheGame(name):
     Syntax = ""
     ar = []
@@ -19,13 +17,19 @@ def hangmanTheGame(name):
         ar.append(randomString)
         characters = methods.character_count(randomString)
 
+        #number of tries
         tries_left = 10
         while tries_left > 0:
             print(tries_left)
             print(randomString)
             print(characters)
             i = input("Guess a character ")
-            tries_left = methods.lives_count(tries_left)
+
+            # if user guess is not in word decrement tries_left by one
+            if i not in randomString:
+                tries_left = methods.lives_count(tries_left)
+            else:
+                print("yaay")
 
             if not i: #If no input is received, stop the game
                 cur.close()
